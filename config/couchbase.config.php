7b98fda9797bf6c38d07c64151dd1e410443a1cf
Teraglehn <teraglehn@gmail.com>
2015-02-23 17:17:45 +0100

    Update couchbase.config.php

--- config/couchbase.config.php
+++ config/couchbase.config.php
@@ -1,9 +1,7 @@
 <?php
 return array(
     'couchbase' => array(
-        'server' => array(
-            '127.0.0.1:8091',
-        ),
+        'server' => '127.0.0.1:8091',
         'user' => '',
         'password' => '',
         'bucket' => 'default'
